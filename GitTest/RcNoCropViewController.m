16180cc81854b8fea2ac199f5b08058ab2ae7aad
rcplatform <rcplatform@rcplatformdeMac-mini-3.local>
2014-04-18 18:38:13 +0800

    11111111

--- GitTest/RcNoCropViewController.m
+++ GitTest/RcNoCropViewController.m
@@ -18,7 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"%@",@"123");
+    NSLog(@"%@",@"123");1111111
 }
 
 - (void)didReceiveMemoryWarning
