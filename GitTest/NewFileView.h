ebc4177a6665d9783116c512b798af18399b15a9
rcplatform <rcplatform@rcplatforms-Mac-mini.local>
2014-04-21 18:10:41 +0800

    newlife

---
+++ GitTest/NewFileView.h
@@ -0,0 +1,13 @@
+//
+//  NewFileView.h
+//  GitTest
+//
+//  Created by rcplatform on 14-4-21.
+//  Copyright (c) 2014年 com.rcplatformhk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewFileView : UIView
+
+@end
