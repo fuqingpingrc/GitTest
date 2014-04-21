5e26cae1232a5616e106778a64bec2a4f91f3329
rcplatform <rcplatform@rcplatforms-Mac-mini.local>
2014-04-21 18:03:52 +0800

    new file

---
+++ GitTest/FirstView.h
@@ -0,0 +1,13 @@
+//
+//  FirstView.h
+//  GitTest
+//
+//  Created by rcplatform on 14-4-21.
+//  Copyright (c) 2014年 com.rcplatformhk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstView : UIViewController
+
+@end
---
+++ GitTest/FirstView.m
@@ -0,0 +1,49 @@
+//
+//  FirstView.m
+//  GitTest
+//
+//  Created by rcplatform on 14-4-21.
+//  Copyright (c) 2014年 com.rcplatformhk. All rights reserved.
+//
+
+#import "FirstView.h"
+
+@interface FirstView ()
+
+@end
+
+@implementation FirstView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
