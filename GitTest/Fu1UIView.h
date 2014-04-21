5dda9c586a1fca4a9a716a76f9a0c6525d8a92b5
rcplatform <rcplatform@rcplatformdeMac-mini-3.local>
2014-04-21 18:09:59 +0800

    fu1

---
+++ GitTest/Fu1UIView.h
@@ -0,0 +1,13 @@
+//
+//  Fu1UIView.h
+//  GitTest
+//
+//  Created by rcplatform on 14-4-21.
+//  Copyright (c) 2014年 com.rcplatformhk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Fu1UIView : UIView
+
+@end
